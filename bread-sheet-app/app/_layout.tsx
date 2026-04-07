49cd1c265c374d9e15d93ae50ecb46ec27f6347b
JanoNick <jano-nick@pm.me>
2026-04-07 18:46:22 +0200

    Fix login for anonymous users

--- bread-sheet-app/app/_layout.tsx
+++ bread-sheet-app/app/_layout.tsx
@@ -13,7 +13,7 @@ export const unstable_settings = {
 const AUTHENTICATED_GROUPS = ['(tabs)', '(account)', '(app)'];
 
 function RootLayoutNav() {
-  const { session, isLoading } = useSession();
+  const { session, isLoading, isAnonymous } = useSession();
   const segments = useSegments();
   const router = useRouter();
 
@@ -21,12 +21,12 @@ function RootLayoutNav() {
     if (isLoading) return;
     const inAuthenticatedGroup = AUTHENTICATED_GROUPS.includes(segments[0] as string);
     const inAuthGroup = segments[0] === '(auth)';
-    if (session && !inAuthenticatedGroup) {
+    if (session && !isAnonymous && !inAuthenticatedGroup) {
       router.replace('/(tabs)');
     } else if (!session && !inAuthGroup) {
       router.replace('/(auth)/login');
     }
-  }, [session, isLoading, segments, router]);
+  }, [session, isAnonymous, isLoading, segments, router]);
 
   if (isLoading) return null;
 
